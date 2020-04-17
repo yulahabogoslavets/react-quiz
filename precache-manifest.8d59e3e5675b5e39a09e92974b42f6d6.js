5ee41a62cf3a7cfc6a19e694ebcce2d5c06a250d
yulahabogoslavets <speed0435@gmail.com>
2020-04-17 16:16:56 +0200

    Updates

---
+++ precache-manifest.8d59e3e5675b5e39a09e92974b42f6d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0629b4d5fa5adbde25f2e65264de06",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "ae4c6627e0b67a62b65c",
+    "url": "/react-quiz/static/css/main.1ebaf685.chunk.css"
+  },
+  {
+    "revision": "0482c601a242b90ace19",
+    "url": "/react-quiz/static/js/2.a55f2347.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-quiz/static/js/2.a55f2347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4c6627e0b67a62b65c",
+    "url": "/react-quiz/static/js/main.f3ecea40.chunk.js"
+  },
+  {
+    "revision": "3ac17e5c6687a3394913",
+    "url": "/react-quiz/static/js/runtime-main.6f69342d.js"
+  }
+]);
